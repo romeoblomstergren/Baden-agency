7f3cd9b4980ca5f276940503e3bf2662657f53e1
romeoblomstergren <romeo.baden.blomstergren@gmail.com>
2026-05-10 13:12:59 +0200

    feat: vessel history, contacts, AI email + actions

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Tally       from './pages/Tally'
 import Stats       from './pages/Stats'
 import Finance     from './pages/Finance'
 import PortInfo    from './pages/PortInfo'
-import SOA        from './pages/SOA'
-import Templates  from './pages/Templates'
-import Vessels    from './pages/Vessels'
+import SOA         from './pages/SOA'
+import Templates   from './pages/Templates'
+import Vessels     from './pages/Vessels'
 import DailyReport from './pages/DailyReport'
+import VesselHistory from './pages/VesselHistory'
+import Contacts    from './pages/Contacts'
 import './styles.css'
 
 function Protected({ children }) {
@@ -42,6 +44,8 @@ export default function App() {
         <Route path="/templates" element={<Protected><Templates/></Protected>}/>
         <Route path="/vessels"   element={<Protected><Vessels/></Protected>}/>
         <Route path="/daily"    element={<Protected><DailyReport/></Protected>}/>
+        <Route path="/contacts" element={<Protected><Contacts/></Protected>}/>
+        <Route path="/vessels/:vesselName" element={<Protected><VesselHistory/></Protected>}/>
         <Route path="*"         element={<Navigate to="/" replace/>}/>
       </Routes>
     </BrowserRouter>
