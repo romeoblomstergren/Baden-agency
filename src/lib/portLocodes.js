3b142968ab9e370835640e762d776a783a48f16f
romeoblomstergren <romeo.baden.blomstergren@gmail.com>
2026-05-11 00:38:54 +0200

    feat: port UN/LOCODE lookup table

---
+++ src/lib/portLocodes.js
@@ -0,0 +1,24 @@
+// src/lib/portLocodes.js
+// Common West/Central Africa ports + major global ports
+export const PORT_LOCODES = [
+  { name: 'Tema',         country: 'Ghana',           locode: 'GHTMA' },
+  { name: 'Abidjan',      country: 'Côte d'Ivoire',  locode: 'CIABJ' },
+  { name: 'Dakar',        country: 'Senegal',          locode: 'SNDKR' },
+  { name: 'Conakry',      country: 'Guinea',           locode: 'GNCKR' },
+  { name: 'Freetown',     country: 'Sierra Leone',     locode: 'SLFTW' },
+  { name: 'Monrovia',     country: 'Liberia',          locode: 'LRMLW' },
+  { name: 'Cotonou',      country: 'Benin',            locode: 'BJCOO' },
+  { name: 'Lagos',        country: 'Nigeria',          locode: 'NGLOS' },
+  { name: 'Douala',       country: 'Cameroon',         locode: 'CMDLA' },
+  { name: 'Libreville',   country: 'Gabon',            locode: 'GALBV' },
+  { name: 'Matadi',       country: 'DRC',              locode: 'CDMAT' },
+  { name: 'Bata',         country: 'Equatorial Guinea',locode: 'GQBSG' },
+  { name: 'Nouakchott',   country: 'Mauritania',       locode: 'MRNKC' },
+  { name: 'Lomé',         country: 'Togo',             locode: 'TGLFW' },
+  { name: 'Pointe-Noire', country: 'Congo',            locode: 'CGPNR' },
+  { name: 'Rotterdam',    country: 'Netherlands',      locode: 'NLRTM' },
+  { name: 'Antwerp',      country: 'Belgium',          locode: 'BEANR' },
+  { name: 'Hamburg',      country: 'Germany',          locode: 'DEHAM' },
+  { name: 'Singapore',    country: 'Singapore',        locode: 'SGSIN' },
+  { name: 'Jebel Ali',    country: 'UAE',              locode: 'AEJEA' },
+]
